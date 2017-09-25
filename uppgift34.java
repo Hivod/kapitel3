865e9c24a77334cb276bb2f551fac060d55291b9
Olof Rydh <ryol16@nti.umea>
2017-09-25 12:11:03 +0200

    förenkling i 34 o 35

--- uppgift34.java
+++ uppgift34.java
@@ -4,12 +4,9 @@ public class uppgift34 {
 
   public static void main(String[] args) {
 
-    String sTime, sPrice;
     double time, price, total;
-    sTime = JOptionPane.showInputDialog("Minuter per m\u00E5nad: ");
-    sPrice = JOptionPane.showInputDialog("Pris per minut: ");
-    time = Double.parseDouble(sTime);
-    price = Double.parseDouble(sPrice);
+    time = Double.parseDouble(JOptionPane.showInputDialog("Minuter per m\u00E5nad: "));
+    price = Double.parseDouble(JOptionPane.showInputDialog("Pris per minut: "));
 
     total = time * price;
 
--- uppgift35.java
+++ uppgift35.java
@@ -4,11 +4,10 @@ public class uppgift35 {
 
   public static void main(String[] args) {
 
-    String sPrice = JOptionPane.showInputDialog("Pris inkl. moms: ");
-    String sTax = JOptionPane.showInputDialog("Moms i %: ");
-    int price = Integer.parseInt(sPrice);
-    int tax = Integer.parseInt(sTax) + 100;
+    int price = Integer.parseInt( JOptionPane.showInputDialog("Pris inkl. moms: "));
+    int tax = Integer.parseInt( JOptionPane.showInputDialog("Moms i %: ")) + 100;
     int preTax = (price * 100) / tax;
+
     System.out.println(preTax);
 
   }
